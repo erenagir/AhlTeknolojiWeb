257ba16103d158c2a7a7c1c0423f49e83bbefe6f
Hakan <95884021+hakancyln@users.noreply.github.com>
2024-01-16 17:38:11 +0300

    deneme push

--- AhlTenoloji.Application/Services/Abstract/IGenericService.vb
+++ AhlTenoloji.Application/Services/Abstract/IGenericService.vb
@@ -3,7 +3,7 @@
 Public Interface IGenericService(Of T)
     Function GetAsync(ByVal Filter As Expression(Of Func(Of T, Boolean)),
                           ParamArray IncludeList As String()) As Task(Of T)
-    Function GetAllAsync(ByVal Optional Filter As Expression(Of Func(Of T, Boolean)) = Nothing, Optional IncludeList As String() = Nothing) As Task(Of IEnumerable(Of Task))
+    Function GetAllAsync(ByVal Optional Filter As Expression(Of Func(Of T, Boolean)) = Nothing, Optional IncludeList As String() = Nothing) As Task(Of IEnumerable(Of T))
     Function AddAsync(ByVal p As T) As Task(Of T)
     Function UpdateAsync(ByVal p As T) As Task
     Function DeleteAsync(ByVal p As T) As Task
